c16583603a96322c81e0b33a2604f20ce2ec30c4
thonglm <laminhthong@live.com>
2016-04-18 21:53:49 -0700

    Current Workspace
    Upload all current code to git

---
+++ Tools/src/NonTechnicaPackage/CreateSubTaskJIRA.java
@@ -0,0 +1,17 @@
+package NonTechnicaPackage;
+
+public class CreateSubTaskJIRA {
+
+	public static final String JIRA_NAME = "";
+	public static final String RESEARCH = "RESEARCH";
+	public static final String CODE = "CODING";
+	public static final String TEST = "TESTING";
+	public static final String DEV = "DEV";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("[" + RESEARCH + "]" + JIRA_NAME);
+
+	}
+
+}
